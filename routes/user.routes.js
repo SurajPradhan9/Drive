5e207bc5c2fc17273db62dadecb546ad496a59ee
Suraj Pradhan <surajpradhan9104@gmail.com>
2025-03-31 11:16:14 +0530

    Home page Created

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -21,12 +23,65 @@ router.post(
     }
 
     const { email, username, password } = req.body;
+
+    const hashPassword = await bcrypt.hash(password, 10);
     const newUser = await userModel.create({
       email,
       username,
-      password,
+      password: hashPassword,
     });
     res.json(newUser);
+  }
+);
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post(
+  "/login",
+  body("password").trim().isLength({ min: 5 }),
+  body("username").trim().isLength({ min: 3 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array(), message: "Invalid Data" });
+    }
+
+    const { username, password } = req.body;
+
+    const user = await userModel.findOne({
+      username: username,
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Username or Password is incorrect" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Username or Password is incorrect" });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie('token',token)
+
+    res.send('Logged in')
   }
 );
 
